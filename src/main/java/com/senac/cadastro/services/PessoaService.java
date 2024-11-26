130938cd9b1cb3530d4ef22156eb1283910e4dc1
LFAbrahao <lfasenac@gmail.com>
2024-11-26 17:29:38 -0300

    adicionado rabbitTemplate no service

--- src/main/java/com/senac/cadastro/services/PessoaService.java
+++ src/main/java/com/senac/cadastro/services/PessoaService.java
@@ -3,6 +3,8 @@ package com.senac.cadastro.services;
 import java.util.List;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.senac.cadastro.entities.Pessoa;
@@ -13,6 +15,9 @@ public class PessoaService {
 
 	private PessoaRepository pessoaRepository;
 	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
 	public PessoaService(PessoaRepository pessoaRepository) {
 		this.pessoaRepository = pessoaRepository;
 	}
@@ -22,6 +27,7 @@ public class PessoaService {
 	}
 	
 	public Pessoa adicionarPessoa(Pessoa pessoa) {
+		rabbitTemplate.convertAndSend("fila-cadastro", pessoa);
 		return pessoaRepository.save(pessoa);
 	}
 	
